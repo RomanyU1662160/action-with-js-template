09430eed0e48ad2a29fcee86fc4ef8315d69422f
Romany <romanyfayiez@hotmail.com>
2024-04-23 22:47:36 +0100

    add inputs to action

--- dist/index.js
+++ dist/index.js
@@ -2735,17 +2735,25 @@ const { wait } = __nccwpck_require__(312)
 async function run() {
   try {
     const ms = core.getInput('milliseconds', { required: true })
+    const nameToGreet = core.getInput('your-name', { required: true })
 
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
+
+    // `wait` is a utility function that waits for the specified number of milliseconds
     await wait(parseInt(ms, 10))
+
     core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
     core.setOutput('time', new Date().toTimeString())
+    core.setOutput(
+      'GreetingMsg',
+      `Hello ${nameToGreet}!, We waited for ${ms} milliseconds! 🎉 `
+    )
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message)
